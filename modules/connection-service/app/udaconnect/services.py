53ff36fae048a42dba06ba75149978b5f8411578
Mahmud Youssef <mahmud.youssef@icloud.com>
2021-09-20 00:05:06 +0200

    finish connection service

--- modules/connection-service/app/udaconnect/services.py
+++ modules/connection-service/app/udaconnect/services.py
@@ -1,15 +1,22 @@
 import logging
+import os
 from datetime import datetime, timedelta
 from typing import Dict, List
 
+import grpc
 from app import db
 from app.udaconnect.models import Connection, Location, Person
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
-from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text
+
+import location_pb2
+import location_pb2_grpc
+import person_pb2
+import person_pb2_grpc
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("udaconnect-api")
+
+GRPC_SERVISER = os.environ("GRPC_URL")
 
 
 class ConnectionService:
@@ -23,14 +30,19 @@ class ConnectionService:
         large datasets. This is by design: what are some ways or techniques to help make this data integrate more
         smoothly for a better user experience for API consumers?
         """
-        locations: List = db.session.query(Location).filter(
-            Location.person_id == person_id
-        ).filter(Location.creation_time < end_date).filter(
-            Location.creation_time >= start_date
-        ).all()
+        channel = grpc.insecure_channel(GRPC_SERVISER)
+
+        location_stub = location_pb2_grpc.LocationService(channel)
+        location_query = location_pb2.LocationQuery(
+            person_id=person_id,
+            creation_time=start_date
+        )
+        locations: List = location_stub.Filter(location_query)
 
         # Cache all users in memory for quick lookup
-        person_map: Dict[str, Person] = {person.id: person for person in PersonService.retrieve_all()}
+        person_stub = person_pb2_grpc.PersonServiceStub(channel)
+        person_query = person_pb2.PersonQuery(id=person_id)
+        person_map: Dict[str, Person] = {person.id: person for person in person_stub.Filter(person_query)}
 
         # Prepare arguments for queries
         data = []
@@ -79,56 +91,3 @@ class ConnectionService:
                 )
 
         return result
-
-
-class LocationService:
-    @staticmethod
-    def retrieve(location_id) -> Location:
-        location, coord_text = (
-            db.session.query(Location, Location.coordinate.ST_AsText())
-            .filter(Location.id == location_id)
-            .one()
-        )
-
-        # Rely on database to return text form of point to reduce overhead of conversion in app code
-        location.wkt_shape = coord_text
-        return location
-
-    @staticmethod
-    def create(location: Dict) -> Location:
-        validation_results: Dict = LocationSchema().validate(location)
-        if validation_results:
-            logger.warning(f"Unexpected data format in payload: {validation_results}")
-            raise Exception(f"Invalid payload: {validation_results}")
-
-        new_location = Location()
-        new_location.person_id = location["person_id"]
-        new_location.creation_time = location["creation_time"]
-        new_location.coordinate = ST_Point(location["latitude"], location["longitude"])
-        db.session.add(new_location)
-        db.session.commit()
-
-        return new_location
-
-
-class PersonService:
-    @staticmethod
-    def create(person: Dict) -> Person:
-        new_person = Person()
-        new_person.first_name = person["first_name"]
-        new_person.last_name = person["last_name"]
-        new_person.company_name = person["company_name"]
-
-        db.session.add(new_person)
-        db.session.commit()
-
-        return new_person
-
-    @staticmethod
-    def retrieve(person_id: int) -> Person:
-        person = db.session.query(Person).get(person_id)
-        return person
-
-    @staticmethod
-    def retrieve_all() -> List[Person]:
-        return db.session.query(Person).all()
